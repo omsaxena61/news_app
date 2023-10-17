86e3d3c9d963d06628b6d23b375866f69f517b07
omsaxena61 <omsaxena61@gmail.com>
2023-10-17 17:49:44 +0530

    NewsBook

---
+++ src/components/Newsitem.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+
+export class Newsitem extends Component {
+  render() {
+    let {title,description,imageUrl,newsUrl} =this.props; //destrcturing
+    return (
+      <div className="my-3">
+          <div className="card" style={{width: "18rem"}}> 
+        <img src={!imageUrl?"https://www.hindustantimes.com/ht-img/img/2023/10/15/1600x900/PTI10-15-2023-000092A-0_1697363274441_1697363341754.jpg":imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">
+              {description}...
+            </p>
+            <a href={newsUrl} className="btn btn-sm btn-dark">
+              Read more...
+            </a>
+          </div>
+         
+        </div>
+        
+         </div>
+    );
+  }
+}
+
+export default Newsitem;
